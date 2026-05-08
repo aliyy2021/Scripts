bfce9190d70197fef7ac8c87d9f55115d277728e
aliyy2021 <77948441+aliyy2021@users.noreply.github.com>
2026-05-08 19:16:26 +0800

    Update sing-box.js

--- sub-store-template/1.12or13/sing-box.js
+++ sub-store-template/1.12or13/sing-box.js
@@ -19,19 +19,19 @@ config.outbounds.map(i => {
   if (['all'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
-  if (['kr-auto'].includes(i.tag)) {
+  if (['🇰🇷 韩国'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置)).*(🇰🇷|KR|kr|韩国|首尔|Korea)/i))
   }
-  if (['ae-auto'].includes(i.tag)) {
+  if (['🇦🇪 阿联酋'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置)).*(🇦🇪|AE|ae|阿联酋|阿拉伯|迪拜)/i))
   }
-  if (['jp-auto'].includes(i.tag)) {
+  if (['🇯🇵 日本'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置)).*(🇯🇵|JP|jp|日本|日|东京|Japan)/i))
   }
-  if (['sg-auto'].includes(i.tag)) {
+  if (['🇸🇬 新加坡'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置)).*(🇸🇬|SG|sg|新加坡|坡|狮城|Singapore)/i))
   }
-  if (['us-auto'].includes(i.tag)) {
+  if (['🇺🇸 美国'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置)).*(🇺🇸|US|us|美国|美|凤凰城|United States)/i))
   }
 })
